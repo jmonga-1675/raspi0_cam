4f5e9b86ba9bb4e2b5b1230613b42ac2cc7a3c3d
Jay Monga <jaymonga16@berkeley.edu>
2021-07-01 06:57:35 +0100

    publish downsampled images to ROS

--- src/camera_node.py
+++ src/camera_node.py
@@ -1,4 +1,4 @@
-#!/usr/env/python
+#!/usr/bin/env python2.7
 # Credit: Adrian Rosebrock
 # https://www.pyimagesearch.com/2015/03/30/accessing-the-raspberry-pi-camera-with-opencv-and-python/
  
@@ -6,10 +6,18 @@
 from picamera.array import PiRGBArray # Generates a 3D RGB array
 from picamera import PiCamera # Provides a Python interface for the RPi Camera Module
 import time # Provides time-related functions
-import cv2 # OpenCV library
+# import cv2 # OpenCV library
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+import rospy
+
+# init node
+rospy.init_node("camera_node")
+pub = rospy.Publisher("kamigami/img_raw", Image, queue_size=10)
  
 # Initialize the camera
 camera = PiCamera()
+bridge = CvBridge()
  
 # Set the camera resolution
 camera.resolution = (640, 480)
@@ -25,19 +33,13 @@ time.sleep(0.1)
  
 # Capture frames continuously from the camera
 for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
+    if rospy.is_shutdown():
+        break
      
     # Grab the raw NumPy array representing the image
-    image = frame.array
-     
-    # Display the frame using OpenCV
-    cv2.imshow("Frame", image)
-     
-    # Wait for keyPress for 1 millisecond
-    key = cv2.waitKey(1) & 0xFF
+    image = frame.array[240:400, 180:300]
+    image_msg = bridge.cv2_to_imgmsg(image, encoding="rgb8")
+    pub.publish(image_msg)
      
     # Clear the stream in preparation for the next frame
     raw_capture.truncate(0)
-     
-    # If the `q` key was pressed, break from the loop
-    if key == ord("q"):
-        break
