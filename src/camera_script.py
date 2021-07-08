73063e7e93ec56c0da976c348b014759888a293f
Jay Monga <jaymonga16@berkeley.edu>
2021-07-08 07:28:32 +0100

    low res high fps tests

--- src/camera_script.py
+++ src/camera_script.py
@@ -7,19 +7,11 @@ from picamera.array import PiRGBArray # Generates a 3D RGB array
 from picamera import PiCamera # Provides a Python interface for the RPi Camera Module
 import time # Provides time-related functions
 import cv2 # OpenCV library
-from cv_bridge import CvBridge
-from sensor_msgs.msg import Image
-import rospy
 
 print("Starting...")
 
-# init node
-rospy.init_node("camera_node")
-pub = rospy.Publisher("kamigami/img_raw", Image, queue_size=1)
- 
 # Initialize the camera
 camera = PiCamera()
-bridge = CvBridge()
  
 # Set the camera resolution
 # camera.resolution = (640, 480)
@@ -39,17 +31,16 @@ time.sleep(0.1)
 # Capture frames continuously from the camera
 counter = 0
 start = time.time()
+counter_lim = 200
 for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
-    if rospy.is_shutdown():
+    counter += 1
+    if counter == counter_lim:
         break
-    # counter += 1
     # Grab the raw NumPy array representing the image
     # image = frame.array[240:400, 180:300]
-    image = frame.array
-    image_msg = bridge.cv2_to_imgmsg(image, encoding="rgb8")
-    pub.publish(image_msg)
+    # image = frame.array
      
     # Clear the stream in preparation for the next frame
     raw_capture.truncate(0)
 end = time.time()
-print("FPS: %f" % (counter/(end - start)))
+print("FPS: %f" % (counter_lim/(end - start)))
